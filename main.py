6fb9d81cf9017f71c625987c90db5caab53ac2f5
MatthewYeager <MatthewBrandonYeager+git@gmail.com>
2019-10-06 14:21:15 -0700

    Profile checkpoint: Generate all single draw combinations

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 ''' Explore 2-7 Triple Draw Statistics '''
 
-import time
+from operator import add
+from time import time
 
 import numpy as np
 
@@ -21,7 +22,29 @@ def cards_display(hand):
     index = 0
     while hand:
         if hand & 0b1:
-            print(Value(Card.value(index)), Suit(Card.color(index)).name[0], end=' ')
+            print(Value(Card.value(index)).name, Suit(Card.color(index)).name[0].lower(), end='; ')
+        index += 1
+        hand = hand >> 1
+
+def cards_value_minimal(hand):
+    index = 0
+    while hand:
+        if hand & 0b1:
+            v = Card.value(index)
+            print('cards_value_minimal', v)
+            if v < 8:
+                yield str(v + 2)
+            elif v == 8:
+                yield 'T'
+            elif v == 9:
+                yield 'J'
+            elif v == 10:
+                yield 'Q'
+            elif v == 11:
+                yield 'K'
+            elif v == 12:
+                yield 'A'
+
         index += 1
         hand = hand >> 1
 
@@ -71,45 +94,34 @@ def rank_hands(h1, h2):
 
     return (0, 0, 1)
 
+def yolo():
+    cards, p1 = cards_from_deck(cards, [
+        (Value.TWO, Suit.CLUBS),
+        (Value.THREE, Suit.HEARTS),
+        (Value.FOUR, Suit.HEARTS),
+        (Value.FIVE, Suit.HEARTS)])
+
+    single_draw_top = [
+        Value.EIGHT, Value.NINE, Value.TEN, Value.JACK, Value.QUEEN, Value.KING, Value.ACE
+    ]
+
+    cards, p2 = cards_from_deck(cards, [
+        (Value.TWO, Suit.DIAMONDS),
+        (Value.THREE, Suit.SPADES),
+        (Value.FOUR, Suit.DIAMONDS),
+        (Value.SEVEN, Suit.SPADES)])
 
 
-cards = Deck.create(52)
-cards, p1 = cards_from_deck(cards, [
-    (Value.TWO, Suit.HEARTS),
-    (Value.THREE, Suit.CLUBS),
-    (Value.FOUR, Suit.HEARTS),
-    (Value.FIVE, Suit.HEARTS)])
+def simulate_deuce_to_seven(t1, p2, cards):
+    trials = 5#25
+    runs = 2000
 
-single_draw_top = [
-    Value.EIGHT, Value.NINE, Value.TEN, Value.JACK, Value.QUEEN, Value.KING, Value.ACE
-]
-
-cards, p2 = cards_from_deck(cards, [
-    (Value.TWO, Suit.CLUBS),
-    (Value.THREE, Suit.SPADES),
-    (Value.FOUR, Suit.HEARTS),
-    (Value.SEVEN, Suit.SPADES)])
-
-from operator import add
-
-trials = 25
-runs = 2000
-
-wallclock = time.time()
-
-
-all_cards_picked = []
-for high_card in single_draw_top:
-    t1 = Card.add(p1, Card.to_index((Suit.HEARTS.value, high_card.value)))
-
+    wallclock = time()
     scores = []
     cards_to_pull = 1
-    for j in range(trials):
+    for _ in range(trials):
         score = (0, 0, 0)
-        #if j and j % 5 == 0:
-        #    print(f'trail {j}/{trials} @ {runs}: {round(np.mean(scores)/runs*100, 3)}% +/- {round(np.std(scores)/runs*100, 3)}, {round(time.time() - wallclock, 3)}')
-
-        for k in range(runs):
+        for _ in range(runs):
             t2 = p2
             for i in range(cards_to_pull):
                 c = Deck.cardPeek(cards, 52 - 9 - i)
@@ -120,9 +132,45 @@ for high_card in single_draw_top:
 
         scores.append(score)
 
-    cards_display(t1)
-    print()
+    print('-'.join(reversed(list(cards_value_minimal(t1)))))
     wins, loses, ties = zip(*scores)
-    print(f'{trials}x{runs}', f'{round(np.mean(wins)/runs*100, 2)}% +/- {round(np.std(wins)/runs*100, 2)}%', f'{round(time.time() - wallclock, 3)}s')
+    print(f'{trials}x{runs}', f'{round((runs-np.mean(loses))/runs*100, 2)}% +/- {round((runs-np.mean(loses))/runs*100, 2)}%', list(map(np.sum, (wins, loses, ties))), f'{round(time() - wallclock, 3)}s')
 
-#print(f'Drawing to {len(set(all_cards_picked))} for {round(len(set(all_cards_picked))/47*100, 1)}%. Ran {round(len(all_cards_picked)/(trials*runs)*100, 1)}%')
+from itertools import combinations
+
+def is_values_straight(values):
+    values = sorted([v.value for v in values])
+    return len(set(values)) == 5 and values [-1] - values[0] == 4
+
+all_combos = list(filter(lambda h: not is_values_straight(h), list(combinations(list(Value), 5))))
+all_combos = sorted(all_combos, key=lambda cs: sorted([c.value for c in cs], reverse=True))
+
+cards = Deck.create(52)
+cards, p2 = cards_from_deck(cards, [
+    (Value.TWO, Suit.DIAMONDS),
+    (Value.THREE, Suit.SPADES),
+    (Value.FOUR, Suit.DIAMONDS),
+    (Value.SEVEN, Suit.SPADES)])
+p1_suits = [Suit.CLUBS, Suit.HEARTS, Suit.HEARTS, Suit.HEARTS, Suit.HEARTS]
+
+
+import cProfile, pstats, io
+pr = cProfile.Profile()
+pr.enable()
+
+for hand in all_combos[-10:]:
+    p1 = 0
+    sim_deck = cards
+    for s, v in zip(p1_suits, hand):
+        c = Card.to_index((s.value, v.value))
+        p1 = Card.add(p1, c)
+        sim_deck = Card.remove(sim_deck, c)
+
+    simulate_deuce_to_seven(p1, p2, sim_deck)
+
+pr.disable()
+s = io.StringIO()
+sortby = 'cumulative'
+ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+ps.print_stats()
+print(s.getvalue())
