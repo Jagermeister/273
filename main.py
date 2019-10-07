42290277e2bdfc8278a1b87cf1ec9488e0445d85
MatthewYeager <MatthewBrandonYeager+git@gmail.com>
2019-10-06 21:17:03 -0700

    Profile completed: Generate all single draw

--- main.py
+++ main.py
@@ -31,7 +31,6 @@ def cards_value_minimal(hand):
     while hand:
         if hand & 0b1:
             v = Card.value(index)
-            print('cards_value_minimal', v)
             if v < 8:
                 yield str(v + 2)
             elif v == 8:
@@ -62,31 +61,35 @@ def is_flush(h):
     return len(set(map(lambda i: Card.color(i), cards_from_hand(h)))) == 1
 
 def is_straight(h):
-    values = sorted(list(map(lambda i: Card.value(i), cards_from_hand(h))))
-    return len(set(values)) == 5 and values [-1] - values[0] == 4
+    values = sorted(list(map(lambda i: i%13, cards_from_hand(h))))
+    return len(set(values)) == 5 and values[-1] - values[0] == 4
+
+def is_values_straight(vs):
+    values = sorted(vs)
+    return len(set(values)) == 5 and values[-1] - values[0] == 4
 
 def unique_value_count(h):
-    return len(set(map(lambda i: Card.value(i), cards_from_hand(h))))
+    return len(set(map(lambda i: i%13, cards_from_hand(h))))
 
 
-def rank_hands(h1, h2):
-    h1c = unique_value_count(h1)
-    h2c = unique_value_count(h2)
+def rank_hands(h2, h2_values_sorted, h1_unique_count, is_h1_flush, is_h1_straight, t1_values_sorted):
+    h1c = h1_unique_count # unique_value_count(h1)
+    h2c = len(set(h2_values_sorted)) # unique_value_count(h2)
     if h1c != h2c:
         return (1, 0, 0) if h1c > h2c else (0, 1, 0)
 
-    h1f = is_flush(h1)
+    h1f = is_h1_flush # is_flush(h1)
     h2f = is_flush(h2)
     if h1f != h2f:
         return (1, 0, 0) if h1f else (0, 1, 0)
 
-    h1s = is_straight(h1)
-    h2s = is_straight(h2)
+    h1s = is_h1_straight # is_straight(h1)
+    h2s = is_values_straight(h2_values_sorted) # is_straight(h2)
     if h1s != h2s:
         return (1, 0, 0) if h1s else (0, 1, 0)
 
-    h1v = sorted(list(set(map(lambda i: Card.value(i), cards_from_hand(h1)))), reverse=True)
-    h2v = sorted(list(set(map(lambda i: Card.value(i), cards_from_hand(h2)))), reverse=True)
+    h1v = t1_values_sorted # sorted(list(set(map(lambda i: i%13, cards_from_hand(h1)))), reverse=True)
+    h2v = h2_values_sorted # sorted(list(set(map(lambda i: i%13, cards_from_hand(h2)))), reverse=True)
 
     for a, b in zip(h1v, h2v):
         if a != b:
@@ -94,31 +97,20 @@ def rank_hands(h1, h2):
 
     return (0, 0, 1)
 
-def yolo():
-    cards, p1 = cards_from_deck(cards, [
-        (Value.TWO, Suit.CLUBS),
-        (Value.THREE, Suit.HEARTS),
-        (Value.FOUR, Suit.HEARTS),
-        (Value.FIVE, Suit.HEARTS)])
-
-    single_draw_top = [
-        Value.EIGHT, Value.NINE, Value.TEN, Value.JACK, Value.QUEEN, Value.KING, Value.ACE
-    ]
-
-    cards, p2 = cards_from_deck(cards, [
-        (Value.TWO, Suit.DIAMONDS),
-        (Value.THREE, Suit.SPADES),
-        (Value.FOUR, Suit.DIAMONDS),
-        (Value.SEVEN, Suit.SPADES)])
-
 
 def simulate_deuce_to_seven(t1, p2, cards):
-    trials = 5#25
+    trials = 15
     runs = 2000
 
     wallclock = time()
     scores = []
     cards_to_pull = 1
+
+    is_t1_flush = is_flush(t1)
+    is_t1_straight = is_straight(t1)
+    t1_unique_count = unique_value_count(t1)
+    t1_values_sorted = sorted(list(set(map(lambda i: i%13, cards_from_hand(t1)))), reverse=True)
+
     for _ in range(trials):
         score = (0, 0, 0)
         for _ in range(runs):
@@ -127,22 +119,22 @@ def simulate_deuce_to_seven(t1, p2, cards):
                 c = Deck.cardPeek(cards, 52 - 9 - i)
                 t2 = Card.add(t2, c)
 
-            result = rank_hands(t1, t2)
+            t2_values_sorted = sorted(list(set(map(lambda i: i%13, cards_from_hand(t2)))), reverse=True)
+            result = rank_hands(t2, t2_values_sorted, t1_unique_count, is_t1_flush, is_t1_straight, t1_values_sorted)
             score = tuple(map(add, score, result))
 
         scores.append(score)
 
-    print('-'.join(reversed(list(cards_value_minimal(t1)))))
     wins, loses, ties = zip(*scores)
-    print(f'{trials}x{runs}', f'{round((runs-np.mean(loses))/runs*100, 2)}% +/- {round((runs-np.mean(loses))/runs*100, 2)}%', list(map(np.sum, (wins, loses, ties))), f'{round(time() - wallclock, 3)}s')
+    print('-'.join(reversed(list(cards_value_minimal(t1)))), f'{round((runs-np.mean(loses))/runs*100, 2):.2f}% +/- {round((np.std(loses))/runs*100, 2):.2f}%', list(map(np.sum, (wins, loses, ties))), f'{round(time() - wallclock, 3)}s')
 
 from itertools import combinations
 
-def is_values_straight(values):
+def is_value_enum_straight(values):
     values = sorted([v.value for v in values])
     return len(set(values)) == 5 and values [-1] - values[0] == 4
 
-all_combos = list(filter(lambda h: not is_values_straight(h), list(combinations(list(Value), 5))))
+all_combos = list(filter(lambda h: not is_value_enum_straight(h), list(combinations(list(Value), 5))))
 all_combos = sorted(all_combos, key=lambda cs: sorted([c.value for c in cs], reverse=True))
 
 cards = Deck.create(52)
@@ -153,12 +145,12 @@ cards, p2 = cards_from_deck(cards, [
     (Value.SEVEN, Suit.SPADES)])
 p1_suits = [Suit.CLUBS, Suit.HEARTS, Suit.HEARTS, Suit.HEARTS, Suit.HEARTS]
 
+#import cProfile, pstats, io
+#pr = cProfile.Profile()
+#pr.enable()
 
-import cProfile, pstats, io
-pr = cProfile.Profile()
-pr.enable()
 
-for hand in all_combos[-10:]:
+for hand in all_combos[:246]:
     p1 = 0
     sim_deck = cards
     for s, v in zip(p1_suits, hand):
@@ -168,9 +160,9 @@ for hand in all_combos[-10:]:
 
     simulate_deuce_to_seven(p1, p2, sim_deck)
 
-pr.disable()
-s = io.StringIO()
-sortby = 'cumulative'
-ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-ps.print_stats()
-print(s.getvalue())
+#pr.disable()
+#s = io.StringIO()
+#sortby = 'cumulative'
+#ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+#ps.print_stats()
+#print(s.getvalue())
